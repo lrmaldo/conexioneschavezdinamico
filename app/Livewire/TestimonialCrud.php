e1dacf0a7c80c7fc794fd9c3f74b5e36254a3fbf
leo maldonado <lrmaldo@gmail.com>
2025-04-16 15:41:47 -0600

    feat: actualizar el logo de la aplicación y agregar componente Livewire para gestión de testimonios

---
+++ app/Livewire/TestimonialCrud.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TestimonialCrud extends Component
+{
+    public function render()
+    {
+        return view('livewire.testimonial-crud');
+    }
+}
